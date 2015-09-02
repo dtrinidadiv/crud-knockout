9bc5a4f0bd5794dba8fc8021037431e650aeabaa
dtrinidadiv <dtrinidadiv.dev@gmail.com>
2015-09-02 15:05:05 +0800

    add templates footer and header

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include "templates/include/header.php" ?>
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+<?php include "templates/include/footer.php" ?>
+  
